44a2d93585c11b2856b07cff9669e8534c393267
sehrishbashir <49878787+sehrishbashir@users.noreply.github.com>
2019-07-13 14:54:10 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# monalresturant
+# tech-datainfo
+My site is published at https://sehrishbashir.github.io/tech-datainfo/.
---
+++ script.js
@@ -0,0 +1,18 @@
+let Monal[
+    {
+        resturentName: "Monal",
+        location: "Islamabad",
+        way: "at Highest peak of mountain",
+        adress:  "9KM Pir Sohawa Rd, Islamabad, Islamabad Capital Territory",
+        openHours: {
+            Saturday	9am–12:30am
+            Sunday	9am–12:30am
+            Monday	9am–12:30am
+            Tuesday	9am–12:30am
+            Wednesday	9am–12:30am
+            Thursday	9am–12:30am
+            Friday	9am–12:30am 
+        },
+        quantityOfVariety: "300",
+        VarietyInBreakfast: { } 
+]
